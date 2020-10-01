708796631a328ef0222b3c193e1843e9cdb4a5c4
Dusty McCord <Dustymccord@gmail.com>
2020-10-01 14:15:53 -0700

    create root reducer.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,14 @@
-import { combineReducers } from 'redux';
-
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import userEventsReducer from './user-events';
 
 const rootReducer = combineReducers({
   userEvents: userEventsReducer,
 });
+
+// Exports root state type for use is other components
+// Infers state from 'ReturnType'
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = createStore(rootReducer);
+
+export default store;
