e16592ad67412c4ce89aebc02c308539a898bcbe
Dusty McCord <Dustymccord@gmail.com>
2020-10-01 14:40:43 -0700

    add Record component.

--- Tutorial/4-creating-recorder-component/src/components/App/App.tsx
+++ Tutorial/4-creating-recorder-component/src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Recorder from '../Recorder';
 import './App.css';
+import Recorder from '../Recorder';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Recorder />
     </div>
   );
-}
+};
 
 export default App;
