9122010e800a023a705bef69a7aa27f7d277384c
Dusty McCord <Dustymccord@gmail.com>
2020-10-28 09:06:40 -0700

    add delete button action.

--- src/redux/user-events.ts
+++ src/redux/user-events.ts
@@ -111,6 +111,37 @@ export const createUserEvent = (): ThunkAction<
   }
 };
 
+const DELETE_REQUEST = 'userEvent/delete_request';
+interface DeleteRequestAction extends Action<typeof DELETE_REQUEST> {}
+
+const DELETE_SUCCESS = 'userEvent/delete_success';
+interface DeleteSuccessAction extends Action<typeof DELETE_SUCCESS> {
+  payload: { id: UserEvent['id'] };
+}
+const DELETE_FAILURE = 'userEvent/delete_failure';
+interface DeleteFailureAction extends Action<typeof DELETE_FAILURE> {}
+
+export const deleteUserEvent = (
+  id: UserEvent['id']
+): ThunkAction<
+  Promise<void>,
+  RootState,
+  undefined,
+  DeleteRequestAction | DeleteSuccessAction | DeleteFailureAction
+> => async (dispatch) => {
+  dispatch({ type: DELETE_REQUEST });
+  try {
+    const response = await fetch(`http://localhost:3001/events/${id}`, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      dispatch({ type: DELETE_SUCCESS, payload: { id } });
+    }
+  } catch (e) {
+    dispatch({ type: DELETE_FAILURE });
+  }
+};
+
 const selectUserEventsState = (rootState: RootState) => rootState.userEvents;
 
 export const selectUserEventsArray = (rootState: RootState) => {
@@ -125,7 +156,7 @@ const initialState: UserEventsState = {
 
 const userEventsReducer = (
   state: UserEventsState = initialState,
-  action: LoadSuccessAction | CreateSuccessAction
+  action: LoadSuccessAction | CreateSuccessAction | DeleteSuccessAction
 ) => {
   switch (action.type) {
     case LOAD_SUCCESS:
@@ -146,6 +177,16 @@ const userEventsReducer = (
         allIds: [...state.allIds, event.id],
         byIds: { ...state.byIds, [event.id]: event },
       };
+
+    case DELETE_SUCCESS:
+      const { id } = action.payload;
+      const newState = {
+        ...state,
+        byIds: { ...state.byIds },
+        allIds: state.allIds.filter((storedId) => storedId !== id),
+      };
+      delete newState.byIds[id];
+      return newState;
 
     default:
       return state;
